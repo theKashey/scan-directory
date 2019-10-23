766814cc374162ba942a4d06a2a4874f9a8cf98c
Anton Korzunov <thekashey@gmail.com>
2019-10-24 09:19:59 +1100

    migrate to babel 7

--- src/index.js
+++ src/index.js
@@ -15,35 +15,45 @@ export const rejectNodeModules = (file, stats) => stats.isDirectory() && file.ma
 
 const acceptAll = () => true;
 
-export const forExt = (ext) => file => {
-  const match = file.match(new RegExp('\.' + ext + '$','i'));
-  return !!match && match[0][0] === '.'
+export const forExt = (ext) => {
+  const reg = new RegExp('\.' + ext + '$', 'i');
+
+  return file => {
+    const match = file.match(reg);
+    return !!match && match[0][0] === '.'
+  };
 };
 
 export default async function (root, accept = acceptAll, reject = rejectNodeModules) {
+  if (!fs.existsSync(root)) {
+    return [];
+  }
 
   const files = [];
 
   async function walk(dir) {
-    if (!fs.existsSync(dir)) {
-      throw new Error(dir + ' does not exists');
-    }
 
     const list = await readdir(dir);
     const blockers = list.map(async file => {
       const fullpath = path.resolve(dir, file);
-      const stat = await stats(fullpath);
+      try {
+        // file could be a symlink
+        const stat = await stats(fullpath);
 
-      if (reject(fullpath, stat)) {
-        return;
-      }
+        if (reject(fullpath, stat)) {
+          return;
+        }
 
-      if (stat.isDirectory()) {
-        return walk(fullpath);
-      }
+        if (stat.isDirectory()) {
+          return walk(fullpath);
+        }
 
-      if (accept(fullpath, stat)) {
-        files.push(fullpath);
+        if (accept(fullpath, stat)) {
+          files.push(fullpath);
+        }
+      } catch (e) {
+        // do nothing
+        // console.error('could not handle', fullpath, e);
       }
     });
     return Promise.all(blockers);
